70f6d31d71cdcc21ff89f2ee8a7f414e8af570dc
Sergiofer1994 <sergiofer1994@gmail.com>
2026-04-09 14:12:05 +0200

    styles: I add basic styles for desktop screens.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,30 +3,32 @@ import { useState } from 'react';
 function Navbar() {
     const [open, setOpen] = useState(false);
     return (
-        <header className='flex flex-col flex-wrap p-4 bg-[#f4f1ec]'>
-            <div className='flex justify-around items-center w-full'>
-                <button onClick={() => setOpen(!open)}>
+        <header className='flex flex-col p-4 bg-[#f4f1ec] lg:flex-row items-center justify-center'>
+            <div className='flex justify-center items-center w-full lg:w-[40%] lg:gap-8'>
+
+                <h1 className='text-2xl font-serif leading-tight font-bold text-gray-700'>Library</h1>
+
+                <button className='lg:hidden' onClick={() => setOpen(!open)}>
                     {open ? 'X' : '☰'}
                 </button>
-                <h1 className='text-2xl font-serif leading-tight font-bold text-gray-700'>Biblioteca</h1>
             </div>
-            
-            {open && (
-            <nav className="flex" onClick={() => setOpen(false)}>
-                <ul className='flex flex-col w-full'>
+
+
+            <nav className={`${open ? 'flex' : 'hidden'} w-full lg:flex justify-center lg:w-[60%]`} >
+                <ul className='flex flex-col lg:flex-row items-center w-[50%]' >
                     <li className='w-full text-center p-2' >
-                        <a href="/">Home</a>
+                        <a href="/" className='text-xl font-medium' onClick={() => setOpen(false)}>Home</a>
                     </li>
                     <li className='w-full text-center p-2'>
-                        <a href="/">Autores</a>
+                        <a href="/" className='text-xl font-medium' onClick={() => setOpen(false)}>Autores</a>
                     </li>
                     <li className='w-full text-center p-2'>
-                        <a href="/">Libros</a>
+                        <a href="/" className='text-xl font-medium' onClick={() => setOpen(false)}>Libros</a>
                     </li>
-            
+
                 </ul>
             </nav>
-            )}
+           
         </header>
     )
 }

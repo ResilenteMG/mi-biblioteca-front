cb97d1644768a8e594b0afbb4f5c2f695444c6a4
Sergiofer1994 <sergiofer1994@gmail.com>
2026-04-09 09:50:38 +0200

    feat: I create basic navbar component.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,33 @@
-function Navbar () {
+import { useState } from 'react';
+
+function Navbar() {
+    const [open, setOpen] = useState(false);
     return (
-         <h1 className="bg-red-500 text-6xl">Hola Sergio</h1>        
-    );
+        <header >
+            <div>
+                <button onClick={() => setOpen(!open)}>
+                    {open ? 'X' : '☰'}
+                </button>
+                <h1>Biblioteca</h1>
+            </div>
+            
+            { (
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Autores</a>
+                    </li>
+                    <li>
+                        <a href="/">Libros</a>
+                    </li>
+            
+                </ul>
+            </nav>
+            )}
+        </header>
+    )
 }
 export default Navbar;

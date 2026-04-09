90bf1b384a8b521cba348aa12571843273eca31e
Sergiofer1994 <sergiofer1994@gmail.com>
2026-04-09 09:28:56 +0200

    feat: I make basic structure to start to work homepage. Also, i install tailwindcss.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthorList from './pages/Authors/AuthorList'; // Importas tu componente
 import AuthorForm from './pages/Authors/AuthorForm';
+import Home from './pages/Home';
 // Importa también el Header de Sergio si ya lo tiene listo
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       {/* Aquí iría el <Header /> de Sergio */}
       <Routes>
         {/* Definimos que la ruta principal o /authors muestre tu lista */}
-        <Route path="/" element={<AuthorList />} />
+        <Route path="/" element={<Home />} />
         <Route path="/authors" element={<AuthorList />} />
         <Route path="/authors/new" element={<AuthorForm />} />
       </Routes>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+function Navbar () {
+    return (
+         <h1 className="bg-red-500 text-6xl">Hola Sergio</h1>        
+    );
+}
+export default Navbar;

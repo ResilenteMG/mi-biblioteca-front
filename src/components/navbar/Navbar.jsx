0144cb443c715962242cdf46124fe3e61d5b3915
Sergiofer1994 <sergiofer1994@gmail.com>
2026-04-09 10:26:16 +0200

    styles: I add basic styles navbar for mobile screens.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,24 +3,24 @@ import { useState } from 'react';
 function Navbar() {
     const [open, setOpen] = useState(false);
     return (
-        <header >
-            <div>
+        <header className='flex flex-col flex-wrap p-4 bg-[#f4f1ec]'>
+            <div className='flex justify-around items-center w-full'>
                 <button onClick={() => setOpen(!open)}>
                     {open ? 'X' : '☰'}
                 </button>
-                <h1>Biblioteca</h1>
+                <h1 className='text-2xl font-serif leading-tight font-bold text-gray-700'>Biblioteca</h1>
             </div>
             
-            { (
-            <nav>
-                <ul>
-                    <li>
+            {open && (
+            <nav className="flex" onClick={() => setOpen(false)}>
+                <ul className='flex flex-col w-full'>
+                    <li className='w-full text-center p-2' >
                         <a href="/">Home</a>
                     </li>
-                    <li>
+                    <li className='w-full text-center p-2'>
                         <a href="/">Autores</a>
                     </li>
-                    <li>
+                    <li className='w-full text-center p-2'>
                         <a href="/">Libros</a>
                     </li>
             

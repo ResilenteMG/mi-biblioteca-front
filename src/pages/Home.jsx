45ad345863fa7b7f2d46a1c67d55901ce3816293
Sergiofer1994 <sergiofer1994@gmail.com>
2026-04-10 11:08:00 +0200

    feat: carousel ended

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Navbar from '../components/navbar/Navbar';
 import Banner from '../components/banner/Banner';
+import Carousel from '../components/carousel/Carousel';
 
 function Home () {
     return (
         <>
             <Navbar />
+            <Carousel />
             <Banner />
+            
         </>
     )
 }
